effbdbf6b5586a9115596dd9fa1b87734fdab7f3
anna-zau <104685927+anna-zau@users.noreply.github.com>
2022-12-29 14:35:56 +0000

    Deploying to gh-pages from @ anna-zau/goit-react-hw-05-movies@ec598f19be80f2aec6bb61b61db3a7d659ceaaf5 🚀

---
+++ static/js/617.468e6ec3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[617],{617:function(e,t,a){a.d(t,{jTe:function(){return i}});var l=a(983);function i(e){return(0,l.w_)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z",clipRule:"evenodd"}}]})(e)}}}]);
+//# sourceMappingURL=617.468e6ec3.chunk.js.map
